5d70e71d0e6c1896692ac26d6f01f56d27d065de
Jonathan Knowles <mail@jonathanknowles.net>
2025-11-06 14:36:22 +0800

    Group constraints within module `Utilities`.

--- library/ROC/ID/Utilities.hs
+++ library/ROC/ID/Utilities.hs
@@ -8,12 +8,12 @@ import Control.Monad.Random.Class
 guard :: x -> Maybe y -> Either x y
 guard x = maybe (Left x) Right
 
-maybeBoundedEnum :: forall a . Bounded a => Enum a => Int -> Maybe a
+maybeBoundedEnum :: forall a. (Bounded a, Enum a) => Int -> Maybe a
 maybeBoundedEnum i
   | i < fromEnum (minBound :: a) = Nothing
   | i > fromEnum (maxBound :: a) = Nothing
   | otherwise                    = pure $ toEnum i
 
-randomBoundedEnum :: forall a m . MonadRandom m => Bounded a => Enum a => m a
+randomBoundedEnum :: forall a m. (MonadRandom m, Bounded a, Enum a) => m a
 randomBoundedEnum =
   toEnum <$> getRandomR (fromEnum (minBound :: a), fromEnum (maxBound :: a))
