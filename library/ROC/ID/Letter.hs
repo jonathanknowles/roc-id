87b019fd9373e2b73614a503fd9f5542cf646a63
Jonathan Knowles <mail@jonathanknowles.net>
2025-12-02 19:55:18 +0800

    Add documentation to `Letter` module.

--- library/ROC/ID/Letter.hs
+++ library/ROC/ID/Letter.hs
@@ -21,13 +21,22 @@ data Letter
   | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
   deriving (Bounded, Enum, Eq, Generic, Ord, Read, Show)
 
+-- | Attempts to parse a 'Letter' from a character.
+--
+-- Returns 'Nothing' if the specified character is not an uppercase alphabetic
+-- character.
+--
 fromChar :: Char -> Maybe Letter
 fromChar c = readMaybe [c]
 
+-- | Converts the specified 'Letter' to a character.
+--
 toChar :: Letter -> Char
 toChar letter = case show letter of
   [c] -> c
   _ -> error "toChar"
 
+-- | Generates a random 'Letter'.
+--
 generate :: MonadRandom m => m Letter
 generate = randomBoundedEnum
