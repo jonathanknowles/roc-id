b7807c631d61812ee40ffee9d120d1e4bcd5ec0a
Jonathan Knowles <mail@jonathanknowles.net>
2025-12-02 12:29:41 +0800

    Add public functions `Serial.{from.to}Tuple`.

--- library/ROC/ID/Serial.hs
+++ library/ROC/ID/Serial.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 
 module ROC.ID.Serial
   ( Serial (..)
+  , fromTuple
+  , toTuple
   , generate
   ) where
 
@@ -25,6 +28,18 @@ import qualified Data.Vector.Sized as V
 --
 newtype Serial = Serial (Vector 7 Digit)
   deriving (Eq, Generic, Ord, Show)
+
+-- | Constructs a 'Serial' number from a tuple.
+--
+fromTuple :: d ~ Digit => (d, d, d, d, d, d, d) -> Serial
+fromTuple = Serial . V.fromTuple
+
+-- | Converts a 'Serial' number to a tuple.
+--
+toTuple :: d ~ Digit => Serial -> (d, d, d, d, d, d, d)
+toTuple (Serial s) = (s!0, s!1, s!2, s!3, s!4, s!5, s!6)
+  where
+    (!) = V.index
 
 -- | Generates a random 'Serial' number.
 --
